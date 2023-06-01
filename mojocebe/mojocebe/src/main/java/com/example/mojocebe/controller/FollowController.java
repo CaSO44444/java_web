5787edf9d5866a38395f19e7f1596de794701266
DawnT0wn <756231955@qq.com>
2023-06-01 20:25:28 +0800

    修改了follow/find参数接受方式

--- mojocebe/mojocebe/src/main/java/com/example/mojocebe/controller/FollowController.java
+++ mojocebe/mojocebe/src/main/java/com/example/mojocebe/controller/FollowController.java
@@ -7,10 +7,7 @@ import com.example.mojocebe.entity.Patient;
 import com.example.mojocebe.service.FollowService;
 import com.example.mojocebe.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -53,7 +50,7 @@ public class FollowController {
     }
 
     @PostMapping("/follow/find")
-    public Result find(Integer id){
+    public Result find(@RequestParam Integer id){
         List<Follow> follow= followService.find(id);
         return new Result().ok(follow);
     }
