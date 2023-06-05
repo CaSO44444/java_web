3dd2d5f3836df24ccd790561e1e19a50564fab8b
CaSO44444 <752156917@qq.com>
2023-06-05 16:50:37 +0800

    jwt获取值修改

--- mojocebe/mojocebe/src/main/java/com/example/mojocebe/controller/JwtController.java
+++ mojocebe/mojocebe/src/main/java/com/example/mojocebe/controller/JwtController.java
@@ -5,14 +5,24 @@ import com.example.mojocebe.utils.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RestController
 public class JwtController {
+    private String token;
     @GetMapping("/checkToken")
     public Result checkToken(HttpServletRequest request) {
-        String token = request.getHeader("token");
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null){
+            for(Cookie cookie : cookies){
+                if (cookie.getName().equals("token")){
+                    token = cookie.getValue();
+                    break;
+                }
+            }
+        }
         Map map = JwtUtils.getMemberIdByJwtToken(token);
         return new Result().ok(map);
     }
